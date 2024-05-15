6987006d3a1eca6b6e51106801ae7c475b918ee1
alessandrodamato <alessandro042004@gmail.com>
2024-05-15 15:58:24 +0200

    [add] migration

---
+++ database/migrations/2024_05_15_135032_create_wines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wines', function (Blueprint $table) {
+            $table->id();
+            $table->string('winery', 50);
+            $table->string('wine', 100);
+            $table->char('rating_average', 4);
+            $table->string('rating_reviews', 15);
+            $table->string('location', 100);
+            $table->string('image');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wines');
+    }
+};
