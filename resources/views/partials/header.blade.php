366ba5dc20abd36b7e918d325e6d2cf65067b507
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-05-23 15:41:51 +0200

    [add] Wine Show

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,19 @@
 <header>
 
-    <nav class="navbar navbar-expand-lg bg-dark">
-        <div class="container">
+  <nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container">
 
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link  text-white " aria-current="page" href="{{ route('home') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link  text-white " aria-current="page" href="{{ route('wines.index') }}">Lista
-                            vini</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link  text-white " aria-current="page" href="{{ route('home') }}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link  text-white " aria-current="page" href="{{ route('wines.index') }}">Lista
+              vini</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </header>
--- resources/views/wines/index.blade.php
+++ resources/views/wines/index.blade.php
@@ -1,24 +1,24 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="container text-white ">
-        <h1 class=" text-center py-4">I Nostri Vini</h1>
+  <div class="container text-white ">
+    <h1 class=" text-center py-4">I Nostri Vini</h1>
 
-        <div class="row row-cols-3">
-            @foreach ($wines as $wine)
-                <div class="col mb-4">
-                    <div class="card h-100 text-center p-3 ">
-                        <img src="{{ $wine->image }}" class="card-img-top" alt="{{ $wine->wine }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $wine->wine }}</h5>
-                            <a href="{{ route('wine-detail', ['id' => $wine->id]) }}" class="btn btn-primary">Info</a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+    <div class="row row-cols-3">
+      @foreach ($wines as $wine)
+        <div class="col mb-4">
+          <div class="card h-100 text-center p-3 ">
+            <img src="{{ $wine->image }}" class="card-img-top" alt="{{ $wine->wine }}">
+            <div class="card-body">
+              <h5 class="card-title">{{ $wine->wine }}</h5>
+              <a href="{{ route('wines.show', $wine) }}" class="btn btn-primary">Info</a>
+            </div>
+          </div>
         </div>
-
-        {{ $wines->links() }}
-
+      @endforeach
     </div>
+
+    {{ $wines->links() }}
+
+  </div>
 @endsection
